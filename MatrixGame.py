c5cda639fd4a0e2165ff1b238d6f585fb90c5d6e
Hippo Maru <hippomaru@ya.ru>
2022-06-11 12:07:15 +0300

    Points system updated

--- MatrixGame.py
+++ MatrixGame.py
@@ -31,24 +31,26 @@ class Player:
 
 class MatrixGame:
 
-    def __init__(self, amount_of_players, players, matrix):
+    def __init__(self, amount_of_players, players, main_matrix, award_matrix):
         self.amount_of_players = amount_of_players
         self.players = players
-        self.matrix = matrix
+        self.main_matrix = main_matrix
+        self.award_matrix = award_matrix
+        self.last_game_choices = []
 
     def __str__(self):
-        return f'Matrix: {self.matrix}\nPlayers: {self.players}'
+        return f'Main matrix: {self.main_matrix}\nAward matrix: {self.award_matrix}\nPlayers: {self.players}'
 
     def __repr__(self):
-        return f'Matrix: {self.matrix}\nPlayers: {self.players}'
+        return f'Main matrix: {self.main_matrix}\nAward matrix: {self.award_matrix}\nPlayers: {self.players}'
 
     def get_result(self):
-        choices = []
+        self.last_game_choices = []
         for player in self.players:
-            choices.append(random.choices([0, 1], weights=[player.get_probability(), 1 - player.get_probability()]))
-        result = self.matrix.copy()
+            self.last_game_choices.append(random.choices([0, 1], weights=[player.get_probability(), 1 - player.get_probability()]))
+        result = self.main_matrix.copy()
         for i in range(self.amount_of_players):
-            choice = choices[i][0]
+            choice = self.last_game_choices[i][0]
             result = result[choice]
         return result[0]
 
@@ -56,10 +58,18 @@ class MatrixGame:
         result_code = result % self.amount_of_players
         return self.players[result_code]
 
+    def get_points(self):
+        result = self.award_matrix.copy()
+        for i in range(self.amount_of_players):
+            choice = self.last_game_choices[i][0]
+            result = result[choice]
+        return result[0]
+
     def play(self, amount_of_games=1):
         if amount_of_games == 1:
             result = self.get_result()
             winner = self.get_winner(result)
+            winner.add_points(self.get_points())
             print(f'The winner is {winner}')
             return winner
         else:
@@ -67,34 +77,35 @@ class MatrixGame:
             for i in range(amount_of_games):
                 result = self.get_result()
                 winner = self.get_winner(result)
-                winner.add_points(result)
+                winner.add_points(self.get_points())
                 winners.append(winner)
             print(f'Statistics: {collections.Counter(winners)}')
             return winners
 
 
-'''examples'''
+'''Hard code example'''
 
-# player1 = Player('Ivan')
-# player1.set_probability(0.33)
-# player2 = Player('Kyrill')
-# player2.set_probability(0.47)
-# player3 = Player('Naruto')
-# player3.set_probability(0.82)
-#
-# players = [player1, player2, player3]
-#
-# mg1 = MatrixGame(3, players, [[[[0], [1]], [[2], [3]]],
-#                               [[[4], [5]], [[6], [7]]]])
-#
-# print(mg1)
-# mg1.play()
-# mg1.play(1000)
-# for player in players:
-#     print(f'Player: {player}\t\tPoints: {player.get_points()}')
-# print(players)
+player1 = Player('Ivan')
+player1.set_probability(0.33)
+player2 = Player('Kyrill')
+player2.set_probability(0.47)
+player3 = Player('Naruto')
+player3.set_probability(0.82)
 
-'''CLI'''
+players = [player1, player2, player3]
+
+mg1 = MatrixGame(3, players, [[[[0], [1]], [[2], [3]]],
+                              [[[4], [5]], [[6], [7]]]], [[[[0], [0]], [[0], [0]]],
+                                                         [[[0], [0]], [[0], [0]]]])
+
+print(mg1)
+mg1.play()
+mg1.play(1000)
+for player in players:
+    print(f'Player: {player}\t\tPoints: {player.get_points()}')
+print(players)
+
+'''CLI example'''
 
 
 def generate_matrix_automatically(matrix, n):
@@ -117,17 +128,23 @@ def generate_matrix_manually(matrix, n):
 
 
 n = int(input('Enter the amount of players: '))
-matrix = [[], []]
+template_matrix = [[], []]
 for i in range(1, n):
-    temp = matrix.copy()
-    matrix = [temp, temp]
-matrix = generate_matrix_automatically(matrix.copy(), n)
+    temp = template_matrix.copy()
+    template_matrix = [temp, temp]
+print("Main matrix will be generated automatically\n")
+matrix = generate_matrix_automatically(template_matrix.copy(), n)
+print("Enter award matrix: ")
+award_matrix = generate_matrix_manually(template_matrix.copy(), n)
 players = []
 for i in range(n):
     name = input(f'Enter the name of player number {i + 1}: ')
     probability = float(input(f'Enter the probability of choosing first of two rows for player {name}: '))
     players.append(Player(name, probability))
-mg = MatrixGame(n, players, matrix)
+mg = MatrixGame(n, players, matrix, award_matrix)
 print(mg)
 k = int(input('Enter the amount of games: '))
 mg.play(k)
+for player in players:
+    print(f'Player: {player}\t\tPoints: {player.get_points()}')
+print(players)
